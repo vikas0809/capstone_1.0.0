627f1b5493dded25a0d621acf717c67135660055
Student <student@students-macbook-pro-2.local>
2016-10-13 15:14:52 -0400

    Initial Commit

---
+++ CapstoneProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CapstoneProject
+//
+//  Created by Student on 2016-10-13.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CapstoneProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CapstoneProject
+//
+//  Created by Student on 2016-10-13.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
